d627921e156c9160fcc444469c0d1a952c52210a
Maxim <sexmilitary@icloud.com>
2020-09-19 20:47:23 +0300

    InsertableTextField

--- iChat/Controllers/ProfileViewController.swift
+++ iChat/Controllers/ProfileViewController.swift
@@ -14,7 +14,7 @@ class ProfileViewController: UIViewController {
     let imageView = UIImageView(image: #imageLiteral(resourceName: "human6"), contentMode: .scaleAspectFill)
     let nameLabel = UILabel(text: "Anna Tkach", font: .systemFont(ofSize: 20, weight: .light))
     let aboutMeLabel = UILabel(text: "You have opportunityto to chat with the best girl in the world!", font: .systemFont(ofSize: 16, weight: .light))
-    let myTextField = UITextField()
+    let myTextField = InsertableTextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +34,14 @@ class ProfileViewController: UIViewController {
         aboutMeLabel.numberOfLines = 0
         containerView.backgroundColor = .mainWhite()
         containerView.layer.cornerRadius = 30
-        myTextField.borderStyle = .roundedRect
+        
+        if let button = myTextField.rightView as? UIButton {
+            button.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
+        }
+    }
+    
+    @objc private func sendMessage() {
+        print(#function)
     }
 }
 
